9585a7ab602c48109d2239e4ffb7d7767ecb2d82
Carlos Blanco <cblancosantero@gmail.com>
2022-03-04 19:29:47 +0100

    Para heroku

--- TFG/settings.py
+++ TFG/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,6 +83,8 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+DATABASES['default'] = dj_database_url.config(default="postgres://alumnodb:alumnodb@localhost:5432/guardian")
+
 
 
 # Password validation
